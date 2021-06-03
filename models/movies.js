1ed00303dc7a163187cb8c7979cda6d11ce8a709
BEASTELMO <beastelmo8@gmail.com>
2021-06-03 02:37:21 -0400

    loading right

--- models/movies.js
+++ models/movies.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const movieSchema = new mongoose.Schema({
     name: { type: String, required: true },
     genre: { type: String, required: true },
-    // streaming: { type: String, required: true },
+    streaming: { type: String, required: true },
+    score: { type: String, required: true },
     hasSeen: Boolean
 });
 
